13b7a468fdbf854cd9fc84145df44fd96a320612
josephgnall <joseph.gnall@scranton.edu>
2017-01-03 13:20:37 -0500

    Test

---
+++ projtrack/src/main/java/edu/ctle/ProjtrackApplication.java
@@ -0,0 +1,12 @@
+package edu.ctle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjtrackApplication.class, args);
+	}
+}
