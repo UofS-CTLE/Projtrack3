2e9bd259993a8d3e656692b4b9cf9e0dfd13998d
Sean Batzel <stbatzel@gmail.com>
2017-01-03 23:34:01 -0500

    Interface is coming together.

--- projtrack/src/main/webapp/scripts/navbar.js
+++ projtrack/src/main/webapp/scripts/navbar.js
@@ -4,27 +4,31 @@ window.onload = function() {
 
   console.log("Navbar loading.");
   var bar = "<table><th>";
-  bar += "<td><button src='home()'>Home</button></td>";
-  bar += "<td><button src='add_project()'>My Projects</button></td>";
-  bar += "<td><button src='my_projects()'>Add Project</button></td>";
-  bar += "<td><button src='all_projects()'>All Projects</button></td>";
+  bar += "<td><button onclick='home()'>Home</button></td>";
+  bar += "<td><button onclick='my_projects()'>My Projects</button></td>";
+  bar += "<td><button onclick='add_project()'>Add Project</button></td>";
+  bar += "<td><button onclick='all_projects()'>All Projects</button></td>";
   bar += "</th></table>";
   document.getElementById("navbar").innerHTML = bar;
   document.getElementById("version").innerHTML = version;
 }
 
 home = function() {
+  console.log("Going home.");
   window.location = "home.html";
 }
 
 add_project = function() {
+  console.log("Adding a project.");
   window.location = "add_project.html";
 }
 
 my_projects = function() {
+  console.log("Navigating to My Projects.");
   window.location = "my_projects.html";
 }
 
 all_projects = function() {
+  console.log("Navigating to All Projects.");
   window.location = "all_projects.html";
 }
