0c15cc8380b90da87a3fa6000f6fda59e02855c9
Sean Batzel <stbatzel@gmail.com>
2017-01-04 12:59:30 -0500

    Why?

---
+++ projtrack/src/main/java/edu/ctle/MvcConfig.java
@@ -0,0 +1,18 @@
+package edu.ctle;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
