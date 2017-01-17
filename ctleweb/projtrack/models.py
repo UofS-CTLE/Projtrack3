4444be9f7cceb90080d29c8109b1857e623b448b
Sean Batzel <stbatzel@gmail.com>
2017-01-17 11:43:08 -0500

    Added a Semesters object.

--- ctleweb/projtrack/models.py
+++ ctleweb/projtrack/models.py
@@ -46,7 +46,15 @@ class Project(models.Model):
     walk_in = models.BooleanField()
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     users = models.ForeignKey(User, on_delete=models.CASCADE)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
     class Meta:
         db_table = u'Project'
     def __str__(self):
         return self.title
+
+class Semester(models.Model):
+    name = models.CharField(max_length=100)
+    class Meta:
+        db_table = u'Semester'
+    def __str__(self):
+        return self.name
