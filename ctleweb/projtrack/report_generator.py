35aed75a46688bf53794058b44a9e364d55d75de
Sean Batzel <stbatzel@gmail.com>
2017-01-20 13:44:21 -0500

    Writing and testing report generation.

--- ctleweb/projtrack/report_generator.py
+++ ctleweb/projtrack/report_generator.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from .models import Project, Semester, User, Client, Department, Type
 
+from django.core.exceptions import ObjectDoesNotExist
+
 def remove_duplicates(t):
     l = []
     for x in t:
@@ -10,53 +12,93 @@ def remove_duplicates(t):
     return l
 
 def check_dates(s_d, e_d):
-    result = list(Project.objects.all())
-    if s_d != '':
-        s_d = datetime.strptime(s_d, "%Y-%m-%d")
-        for x in result:
-            if x.date > s_d:
-                result.remove(x)
-    if e_d != '':
-        e_d = datetime.strptime(e_d, "%Y-%m-%d")
-        for x in result:
-            if x.date < e_d:
-                result.remove(x)
-    return result
+    try:
+        result = list(Project.objects.all())
+        if s_d != '':
+            s_d = datetime.strptime(s_d, "%Y-%m-%d")
+            for x in result:
+                if x.date > s_d:
+                    result.remove(x)
+        if e_d != '':
+            e_d = datetime.strptime(e_d, "%Y-%m-%d")
+            for x in result:
+                if x.date < e_d:
+                    result.remove(x)
+        return result
+    except ObjectDoesNotExist:
+        return []
 
-def check_semester(semester):
-    if semester != '':
-        sem = Semester.objects.get(name=str(semester))
-        return list(Project.objects.get(semester=sem))
-    else:
-        return list(Project.objects.all())
+def check_semester(sem):
+    try:
+        if sem != '':
+            try:
+                return list(Project.objects.get(semester=sem))
+            except TypeError:
+                return Project.objects.get(semester=sem)
+        else:
+            try:
+                return list(Project.objects.all())
+            except TypeError:
+                return Project.objects.all()
+    except ObjectDoesNotExist:
+        return []
 
-def check_user(user):
-    if user != '':
-        use = User.objects.get(username=str(user))
-        return list(Project.objects.get(users=use))
-    else:
-        return list(Project.objects.all())
+def check_user(use):
+    try:
+        if use != '':
+            #use = User.objects.get(username=str(user))
+            return list(Project.objects.get(users=use))
+        else:
+            return list(Project.objects.all())
+    except ObjectDoesNotExist:
+        return []
 
 def check_client(cli):
-    if cli != '':
-        #cli = Client.objects.get(name=str(client))
-        return list(Project.objects.get(client=cli))
-    else:
-        return list(Project.objects.all())
+    try:
+        if cli != '':
+            try:
+                return list(Project.objects.get(client=cli))
+            except TypeError:
+                return Project.objects.get(client=cli)
+        else:
+            try:
+                return list(Project.objects.all())
+            except TypeError:
+                return Project.objects.all()
+    except ObjectDoesNotExist:
+        return []
 
-def check_department(dept):
-    if dept != '':
-        depart = Department.objects.get(name=str(dept))
-        return list(Project.objects.get(department=depart))
-    else:
-        return list(Project.objects.all())
+def check_department(depart):
+    try:
+        if depart != '':
+            try:
+                cli = Client.objects.get(department=depart)
+                return list(Project.objects.get(client=cli))
+            except TypeError:
+                cli = Client.objects.get(department=depart)
+                return Project.objects.get(client=cli)
+        else:
+            try:
+                return list(Project.objects.all())
+            except TypeError:
+                return Project.objects.all()
+    except ObjectDoesNotExist:
+        return []
 
-def check_type(proj_type):
-    if proj_type != '':
-        proj = Type.objects.get(name=str(proj_type))
-        return list(Project.objects.get(type=proj))
-    else:
-        return list(Project.objects.all())
+def check_type(proj):
+    try:
+        if proj != '':
+            try:
+                return list(Project.objects.get(type=proj))
+            except TypeError:
+                return Project.objects.get(type=proj)
+        else:
+            try:
+                return list(Project.objects.all())
+            except TypeError:
+                return Project.objects.all()
+    except ObjectDoesNotExist:
+        return []
 
 def generate_report(req):
     report = list()
--- ctleweb/projtrack/tests.py
+++ ctleweb/projtrack/tests.py
@@ -2,6 +2,7 @@ import django.test
 from projtrack.models import Client, Project, Type, User, Department, Semester
 from django.contrib.auth.models import User as App_User
 import re, datetime
+from .report_generator import generate_report, check_semester, check_client, check_dates, check_user, check_department, check_type, remove_duplicates
 
 
 class DepartmentTestCase(django.test.TestCase):
@@ -100,3 +101,83 @@ class TestLoggedIn(django.test.TestCase):
     def test_logged_in(self):
         response = self.client.post("/home/", follow=True)
         self.assertContains(response, "Home", status_code=200)
+
+
+class TestReportGenerator(django.test.TestCase):
+    def setUp(self):
+        self.test_list = [1,2,1,3,2,4,3,5]
+        Project.objects.create(title="Test",
+                               description="Test",
+                               date=datetime.date.today(),
+                               type=Type.objects.create(name="Project"),
+                               walk_in=False,
+                               client=Client.objects.create(first_name="Bob",
+                                                     last_name="Roberts",
+                                                     department=Department.objects.create(name="Testing"),
+                                                     email='roberts@email.com'),
+                               users=User.objects.create(username="admin"),
+                               semester=Semester.objects.create(name="Test"))
+        Project.objects.create(title="Test",
+                               description="Test",
+                               date=datetime.date.today(),
+                               type=Type.objects.create(name="Test"),
+                               walk_in=False,
+                               client=Client.objects.create(first_name="Jerry",
+                                                     last_name="Jerries",
+                                                     department=Department.objects.create(name="Science"),
+                                                     email='jerries@email.com'),
+                               users=User.objects.create(username="techconbob"),
+                               semester=Semester.objects.create(name="Test2"))
+
+    def test_remove_duplicates(self):
+        self.assertEqual(remove_duplicates(self.test_list), [1,2,3,4,5])
+
+    def test_check_dates(self):
+        pass
+
+    def test_check_semester(self):
+        sem = Semester.objects.get(name="Test")
+        self.assertEqual(check_semester(sem),
+                         Project.objects.get(semester=sem))
+
+    def test_semester_get(self):
+        sem = Semester.objects.get(name="Test")
+        self.assertNotEqual('', str(sem))
+
+    def test_get_user(self):
+        use = User.objects.get(username="techconbob")
+        self.assertNotEqual('', str(use))
+
+    def test_check_user(self):
+        use = User.objects.get(username="techconbob")
+        sem = Semester.objects.get(name="Test2")
+        self.assertEqual(check_semester(sem),
+                         Project.objects.get(users=use))
+
+    def test_get_client(self):
+        cli = Client.objects.get(email="roberts@email.com")
+        self.assertNotEqual('', str(cli))
+
+    def test_check_client(self):
+        sem = Client.objects.get(email="roberts@email.com")
+        self.assertEqual(check_client(sem),
+                         Project.objects.get(client=sem))
+
+    def test_get_department(self):
+        dept = Department.objects.get(name="Science")
+        self.assertNotEqual('', str(dept))
+
+    def test_check_department(self):
+        sci = Department.objects.get(name="Science")
+        cli = Client.objects.get(department=sci)
+        self.assertEqual(check_department(sci),
+                         Project.objects.get(client=cli))
+
+    def test_get_type(self):
+        typ = Type.objects.get(name="Project")
+        self.assertNotEqual('', str(typ))
+
+    def test_check_type(self):
+        sem = Type.objects.get(name="Project")
+        self.assertEqual(check_type(sem),
+                         Project.objects.get(type=sem))
