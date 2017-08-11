bcb0786809cb52770b622cc1fd5ce6d3df9262cb
Sean <stbatzel@gmail.com>
2017-08-10 21:14:09 -0400

    Removed a redundant statement from the build script.
    Signed-off-by: Sean <stbatzel@gmail.com>

--- ctleweb/build.sh
+++ ctleweb/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ -n '/bin/python3' ]; then
-	alias python=python
-fi
-
 function script_help {
     echo This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or \(at your option\) any later version.
     echo
