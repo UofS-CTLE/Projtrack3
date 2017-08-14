a95e0407e634e9a01880fb973b10ca8924e532c6
Sean Batzel <stbatzel@gmail.com>
2017-08-14 09:55:06 -0400

    Javascript performance improvements.
    Namely, I deleted a whole lot of javascript that wasn't doing its job anyway. It was taking up space so I figured we could probably do without.

--- ctleweb/projtrack/static/projtrack/scripts/page.js
+++ ctleweb/projtrack/static/projtrack/scripts/page.js
@@ -1,19 +1,7 @@
 window.onload = function() {
     console.log("Footer loading.");
-    var version = "3.0.3-beta.8";
-    var date = "6/20/2017";
+    var version = "3.0.3";
+    var date = "8/14/2017";
     document.getElementById("version").innerHTML = version;
     document.getElementById("date").innerHTML = date;
-    var url = window.location.href;
-    console.log(url);
-    if ((url).includes("localhost")) {
-        console.log("Modifying form elements.");
-        for (var i = 0; i < document.forms.length; i++) {
-            var action = document.forms[i].action.split("/");
-            console.log(action);
-            action.splice(3,1);
-            console.log(action);
-            document.forms[i].action = action.join("/");
-        }
-    }
 }
