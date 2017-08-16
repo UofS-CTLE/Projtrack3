b6e7eadb285036a4a59dee9c3d04a1fae5870e47
Sean Batzel <stbatzel@gmail.com>
2017-08-16 11:22:01 -0400

    why

--- ctleweb/projtrack/report_generator.py
+++ ctleweb/projtrack/report_generator.py
@@ -170,7 +170,6 @@ def generate_stats(report):
     for x in list(Department.objects.all()):
         proj = 0
         for y in list(Project.objects.all()):
-            current = y.client
             if y.client.department.name == x.name:
                 proj += 1
         depts[x.name] = proj
