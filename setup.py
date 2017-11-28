9398a17e83c6412c188d1f32bb1c2a4e512a472a
Romulus10 <romulus108@protonmail.com>
2017-11-28 09:23:37 -0500

    Update setup.py (#128)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Projtrack3',
-    version='3.0.3',
+    version='3.1.0',
     packages=['ctleweb.ctleweb', 'ctleweb.projtrack', 'ctleweb.projtrack.migrations'],
     url='ctleweb.scranton.edu/projtrack3/',
     license='GPL',
